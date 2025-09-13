0a0161ed321fa9e054dad29f1a1713d0b4442186
Fedorchuk Nataliia <fedorchuk1978nata@gmail.com>
2025-09-13 11:01:54 +0300

    hw-2

---
+++ src/db/models/contact.js
@@ -0,0 +1,34 @@
+import { model, Schema  } from 'mongoose';
+
+const contactSchema = new Schema(
+{
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+email: {
+    type: String,
+},
+isFavourite: {
+    type: Boolean,
+    default: false,
+},
+contactType: {
+    type: String,
+    enum: ['work', 'home', 'personal'],
+    required: true,
+    default: 'personal'
+},
+},
+{
+    timestamps: true,
+    versionKey: false, 
+},
+);
+
+export const ContactsCollection = model('Contact', contactSchema,  'contacts');
+console.log("ContactsCollection uses collection:", ContactsCollection.collection.name);
---
+++ src/server.js
@@ -0,0 +1,77 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+export const setupServer = () => {
+
+  const app = express();
+  const PORT = Number(process.env.PORT) || 3000;
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res, next) => {
+    try {
+      const contacts = await getAllContacts();
+
+      res.status(200).json({
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+      res.status(200).json({
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+    next();
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(500).json({
+      message: 'Interval Server Error',
+    });
+    next(err);
+    return app;
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
