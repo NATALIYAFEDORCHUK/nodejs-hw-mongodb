9762d7a948f2362d89a2aec61074f74845514e12
Fedorchuk Nataliia <fedorchuk1978nata@gmail.com>
2025-10-01 22:42:54 +0300

    add-isFavourite

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,10 @@
-
-import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
+import { isValidObjectId } from 'mongoose';
+
 
 export const isValidId = (req, res, next) => {
-  const { studentId } = req.params;
-  if (!isValidObjectId(studentId)) {
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
     throw createHttpError(400, 'Bad Request');
   }
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,14 +4,14 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(2).max(16).required(),
   email: Joi.string().email(),
-//   isFavourite: Joi.boolean().default(false),
-  contactType: Joi.string().valid('male', 'female', 'other').required(),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string().valid('male', 'female', 'other').required().default('personal'),
 });
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(2).max(16).required(),
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string().min(2).max(16),
     email: Joi.string().email(),
-    // isFavourite: Joi.boolean().default(false),
-    contactType: Joi.string().valid('male', 'female', 'other').required(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('male', 'female', 'other').default('personal'),
   });
