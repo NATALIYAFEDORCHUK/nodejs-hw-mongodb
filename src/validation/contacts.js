d733902da45f979caaa6473943b3b7130cbe7e33
Fedorchuk Nataliia <fedorchuk1978nata@gmail.com>
2025-10-02 22:36:14 +0300

    fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,7 +5,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().min(2).max(16).required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean().default(false),
-  contactType: Joi.string().valid('male', 'female', 'other').required().default('personal'),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
@@ -13,5 +13,5 @@ export const updateContactSchema = Joi.object({
     phoneNumber: Joi.string().min(2).max(16),
     email: Joi.string().email(),
     isFavourite: Joi.boolean(),
-    contactType: Joi.string().valid('male', 'female', 'other').default('personal'),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
   });
